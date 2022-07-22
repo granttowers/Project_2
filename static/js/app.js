70e0599514c8272400f8d26e5aa9cf205b8e7640
Nicholas McMahon (Corporate) <NMcMahon@nsrltd.com>
2022-07-22 09:50:39 +0800

    added bar graph to app,js

--- static/js/app.js
+++ static/js/app.js
@@ -25,62 +25,8 @@ function DemographicInfo(id) {
 }
 init();
 
-// Chart.js Details
 
-function Buildbar(id) {
-    d3.json("/api/v1.0/incident_details").then(data => {
-        var labellist = {};
-        var graphInfo = data;
-        graphInfo = graphInfo.filter(row => row.mine_id == id);
-        graphInfo.forEach(i => {
-            if (i.incident_category in labellist) { labellist[i.incident_category] += 1; }
-            else { labellist[i.incident_category] = 1; }
-        });
-        console.log(labellist);
-        var ctx = document.getElementById("myChart").getContext("2d");
-        var myChart = new Chart(ctx, {
-            type: "bar",
-            data: {
-                labels: Object.keys(labellist),
-                datasets: [
-                    {
-                        label: "Top Incident Catergories",
-                        data: Object.values(labellist),
-                        backgroundColor: [
-                            "rgba(255, 99, 132, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(255, 159, 64, 0.2)"
-                        ],
-                        borderColor: [
-                            "rgba(255,99,132,1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)"
-                        ],
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    yAxes: [
-                        {
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }
-                    ]
-                }
-            }
-        });
-    }
-    )
-};
+
 function buildPie(id) {
     d3.json("/api/v1.0/injury_details").then(data => {
         var bodypart = {};
@@ -99,66 +45,93 @@ function buildPie(id) {
                 else { nature[i.injury_nature] = 1; }
             });
 
-        console.log(bodypart);
-        var data = [{
-            values: Object.values(bodypart),
-            labels: Object.keys(bodypart),
-            domain: { column: 0 },
-            name: 'Injuries by Body Part',
-            hoverinfo: 'label+percent',
-            hole: .4,
-            type: 'pie'
-        }, {
-            values: Object.values(nature),
-            labels: Object.keys(nature),
-            text: '',
-            textposition: 'inside',
-            domain: { column: 1 },
-            name: 'Injuries by Nature',
-            hoverinfo: 'label+percent',
-            hole: .4,
-            type: 'pie'
-        }];
+            console.log(bodypart);
+            var data = [{
+                values: Object.values(bodypart),
+                labels: Object.keys(bodypart),
+                domain: { column: 0 },
+                name: 'Injuries by Body Part',
+                hoverinfo: 'label+percent',
+                hole: .4,
+                type: 'pie'
+            }, {
+                values: Object.values(nature),
+                labels: Object.keys(nature),
+                text: '',
+                textposition: 'inside',
+                domain: { column: 1 },
+                name: 'Injuries by Nature',
+                hoverinfo: 'label+percent',
+                hole: .4,
+                type: 'pie'
+            }];
 
-        var layout = {
-            title: 'Injuries by Body Part and Nature',
-            annotations: [
-                {
-                    font: {
-                        size: 20
+            var layout = {
+                title: 'Injuries by Body Part and Nature',
+                annotations: [
+                    {
+                        font: {
+                            size: 20
+                        },
+                        showarrow: false,
+                        text: "",
+                        x: 0.17,
+                        y: 0.5
                     },
-                    showarrow: false,
-                    text:"",
-                    x: 0.17,
-                    y: 0.5
-                },
-                {
-                    font: {
-                        size: 20
-                    },
-                    showarrow: false,
-                    text:"",
-                    x: 0.82,
-                    y: 0.5
-                }
-            ],
-            height: 400,
-            width: 600,
-            showlegend: false,
-            grid: { rows: 1, columns: 2 }
+                    {
+                        font: {
+                            size: 20
+                        },
+                        showarrow: false,
+                        text: "",
+                        x: 0.82,
+                        y: 0.5
+                    }
+                ],
+                height: 400,
+                width: 600,
+                showlegend: false,
+                grid: { rows: 1, columns: 2 }
+            };
+
+            Plotly.newPlot('pie', data, layout);
+
+        }
+        )
+    }
+    )
+};
+
+function BuildHoriBar(id) {
+    d3.json("/api/v1.0/incident_details").then(data => {
+        var labellist = {};
+        var graphInfo1 = data;
+        graphInfo1 = graphInfo1.filter(row => row.mine_id == id);
+        graphInfo1.forEach(i => {
+            if (i.incident_category in labellist) { labellist[i.incident_category] += 1; }
+            else { labellist[i.incident_category] = 1; }
+        });
+        var traceBar = {
+            x: Object.keys(labellist),
+            y: Object.values(labellist),
+            type: "bar",
+            marker: {color: 'gold'}
         };
 
-        Plotly.newPlot('pie', data, layout);
+        var dataBar = [traceBar];
+        var layoutBar = {
+            title: "Top Incident Categories"
+        };
+        Plotly.newPlot("hbar", dataBar, layoutBar);
 
-    }
-     )
+    });
 }
-    )
- };
+
+
 
 function optionChanged(id) {
     DemographicInfo(id);
-    Buildbar(id);
+    BuildHoriBar(id);
     buildPie(id);
 };
 
